ba4df04be096097059545b7d9dd05066ad79c5dc
tim-arney <85653684+tim-arney@users.noreply.github.com>
2024-11-02 01:56:50 +1100

    Added test mode to app, bypassing loading the dataset

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import argparse
+
 from dash import Dash
 import dash_bootstrap_components as dbc
 
@@ -14,7 +16,7 @@ from sampledata.plot_configs import sample_plot_configs
 APP_NAME = "Network in Progress"
 
 
-def create_app(analytics_manager) -> Dash:
+def create_app(plot_configs) -> Dash:
     """
     Initialize and configure the Dash application.
 
@@ -28,10 +30,6 @@ def create_app(analytics_manager) -> Dash:
         suppress_callback_exceptions=True,
     )
     app.title = APP_NAME
-
-    # @tim: FIXME: Remove sample_plot_configs before submission?
-    plot_configs = sample_plot_configs
-    plot_configs |= analytics_manager.run_analytics()
 
     # Create category structure for tabs and retrieve mappings
     categories, category_key_mapping, subcategory_key_mapping = (
@@ -51,8 +49,8 @@ def create_app(analytics_manager) -> Dash:
 
 
 if __name__ == "__main__":
-    # # Parse command-line arguments
-    # parser = argparse.ArgumentParser()
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
     # parser.add_argument("data", help="Path to the data file")
     # parser.add_argument("mapper", help="Path to the mapper file")
     # parser.add_argument("model", help="Path to the model file")
@@ -60,23 +58,33 @@ if __name__ == "__main__":
     #     "schema", help="Path to the schema file", nargs="?", default=None
     # )
     # parser.add_argument("-d", "--debug", help="Enable debug mode", action="store_true")
-    # args = parser.parse_args()
-
-    # # Load the data
-    # db = dbmgr.DBManager(args.data, args.mapper, args.model, args.schema)
+    parser.add_argument("-t", "--test", help=argparse.SUPPRESS, action="store_true")
+    args = parser.parse_args()
 
     # Hard-coded version for convenience during development
-    DATA = "../datasets/bts_site_b_train/train.zip"
-    MAPPER = "../datasets/bts_site_b_train/mapper_TrainOnly.csv"
-    MODEL = "../datasets/bts_site_b_train/Site_B.ttl"
-    SCHEMA = "../datasets/bts_site_b_train/Brick_v1.2.1.ttl"
     DEBUG = True
 
-    # Load the data
-    db = DBManager(DATA, MAPPER, MODEL, SCHEMA)
+    if not args.test:
+        # # Load the data
+        # db = dbmgr.DBManager(args.data, args.mapper, args.model, args.schema)
 
-    # Load the analytics manager
-    am = AnalyticsManager(db)
+        # Hard-coded version for convenience during development
+        DATA = "../datasets/bts_site_b_train/train.zip"
+        MAPPER = "../datasets/bts_site_b_train/mapper_TrainOnly.csv"
+        MODEL = "../datasets/bts_site_b_train/Site_B.ttl"
+        SCHEMA = "../datasets/bts_site_b_train/Brick_v1.2.1.ttl"
+
+        # Load the data
+        db = DBManager(DATA, MAPPER, MODEL, SCHEMA)
+
+        # Load the analytics manager
+        am = AnalyticsManager(db)
+
+        # @tim: FIXME: Remove sample_plot_configs before submission?
+        plot_configs = sample_plot_configs
+        plot_configs |= am.run_analytics()
+    else:
+        plot_configs = sample_plot_configs
 
     # Run the Dash app server with debug mode enabled
-    create_app(am).run_server(debug=DEBUG)
+    create_app(plot_configs).run_server(debug=DEBUG)
