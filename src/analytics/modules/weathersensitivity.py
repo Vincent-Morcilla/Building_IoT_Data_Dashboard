308bc9887c0fbbfbb2383f42293e557e532a6a73
kripau <kripaupretis@gmail.com>
2024-11-07 15:21:28 +1100

    converted power to func

--- src/analytics/modules/weathersensitivity.py
+++ src/analytics/modules/weathersensitivity.py
@@ -48,21 +48,38 @@ def get_electric_energy_query_str():
             ORDER BY ?meter
             """
 
-electric_power_query_str = """
-SELECT ?meter ?sensor ?stream_id ?phase_count ?phases ?unit ?power_complexity ?power_flow
-WHERE {
-    ?sensor rdf:type brick:Electrical_Power_Sensor .
-    ?meter rdf:type brick:Electrical_Meter .
-    ?sensor brick:isPointOf ?meter .
-    ?sensor senaps:stream_id ?stream_id .
-    OPTIONAL { ?sensor brick:electricalPhaseCount [ brick:value ?phase_count ] . }
-    OPTIONAL { ?sensor brick:electricalPhases [ brick:value ?phases ] . }
-    OPTIONAL { ?sensor brick:hasUnit [ brick:value ?unit ] . }
-    OPTIONAL { ?sensor brick:powerComplexity [ brick:value ?power_complexity ] . }
-    OPTIONAL { ?sensor brick:powerFlow [ brick:value ?power_flow ] . }
-}
-ORDER BY ?meter
-"""
+# electric_power_query_str = """
+# SELECT ?meter ?sensor ?stream_id ?phase_count ?phases ?unit ?power_complexity ?power_flow
+# WHERE {
+#     ?sensor rdf:type brick:Electrical_Power_Sensor .
+#     ?meter rdf:type brick:Electrical_Meter .
+#     ?sensor brick:isPointOf ?meter .
+#     ?sensor senaps:stream_id ?stream_id .
+#     OPTIONAL { ?sensor brick:electricalPhaseCount [ brick:value ?phase_count ] . }
+#     OPTIONAL { ?sensor brick:electricalPhases [ brick:value ?phases ] . }
+#     OPTIONAL { ?sensor brick:hasUnit [ brick:value ?unit ] . }
+#     OPTIONAL { ?sensor brick:powerComplexity [ brick:value ?power_complexity ] . }
+#     OPTIONAL { ?sensor brick:powerFlow [ brick:value ?power_flow ] . }
+# }
+# ORDER BY ?meter
+# """
+
+def get_electric_power_query_str():
+    return """
+            SELECT ?meter ?sensor ?stream_id ?phase_count ?phases ?unit ?power_complexity ?power_flow
+            WHERE {
+                ?sensor rdf:type brick:Electrical_Power_Sensor .
+                ?meter rdf:type brick:Electrical_Meter .
+                ?sensor brick:isPointOf ?meter .
+                ?sensor senaps:stream_id ?stream_id .
+                OPTIONAL { ?sensor brick:electricalPhaseCount [ brick:value ?phase_count ] . }
+                OPTIONAL { ?sensor brick:electricalPhases [ brick:value ?phases ] . }
+                OPTIONAL { ?sensor brick:hasUnit [ brick:value ?unit ] . }
+                OPTIONAL { ?sensor brick:powerComplexity [ brick:value ?power_complexity ] . }
+                OPTIONAL { ?sensor brick:powerFlow [ brick:value ?power_flow ] . }
+            }
+            ORDER BY ?meter
+            """
 
 gas_query_str = """
 SELECT ?meter ?sensor ?stream_id
